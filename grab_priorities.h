205abc15b636d6ef1ed4bddb76609f45ac75bb7e
Carson Black <uhhadd@gmail.com>
2020-06-24 22:43:25 -0400

    XDG Popup Grabs

---
+++ grab_priorities.h
@@ -0,0 +1,20 @@
+/*
+    KWin - the KDE window manager
+    This file is part of the KDE project.
+
+    SPDX-FileCopyrightText: 2020 Carson Black <uhhadd@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+namespace KWin
+{
+
+namespace GrabPriorities
+{
+
+    static const int XdgPopup = 50;
+
+}
+
+}
--- xdgshellclient.cpp
+++ xdgshellclient.cpp
@@ -31,6 +31,8 @@
 #include <KWaylandServer/xdgdecoration_v1_interface.h>
 #include <KWaylandServer/xdgshell_interface.h>
 
+#include "grab_priorities.h"
+
 using namespace KWaylandServer;
 
 namespace KWin
@@ -1726,7 +1728,7 @@ bool XdgPopupClient::hasPopupGrab() const
     if (m_grabSeat.isNull()) {
         return false;
     }
-    return m_grabSeat->grabHandler<XdgPopupGrab>()->toplevelPopup() == m_shellSurface;
+    return m_grabSeat->grabHandler<XdgPopupGrab, GrabPriorities::XdgPopup>()->toplevelPopup() == m_shellSurface;
 }
 
 void XdgPopupClient::popupDone()
@@ -1984,7 +1986,7 @@ void XdgPopupClient::handleGrabRequested(SeatInterface *seat, quint32 serial)
 {
     Q_UNUSED(serial)
     m_grabSeat = seat;
-    seat->grabHandler<XdgPopupGrab>()->grabPopup(m_shellSurface);
+    seat->grabHandler<XdgPopupGrab, GrabPriorities::XdgPopup>()->grabPopup(m_shellSurface);
 }
 
 void XdgPopupClient::initialize()
