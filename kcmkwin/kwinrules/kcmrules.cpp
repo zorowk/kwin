0eb3f24d2ec89f76522d0e919a1822af4cb6045c
Ismael Asensio <isma.af@gmail.com>
2020-10-23 21:08:26 +0200

    [kwinrules] RFC: Update from window menu when the KCM is already open
    By connecting to `ConfigModule::updateArguments()` signal,
    the window UUID passed by `Edit .. specific properties` menu
    can be processed when the KCM is already open.
    
    This allows for subsequent calls from different windows into
    the same KCM instance. If the same window is called twice,
    the corresponding rule will be detected by the matching mechanism.
    
    Requires KConfigWidgets/KDeclarative/KCMUtils patches to implement
    the dBus method and pass the signal around
    
    BUG: 417923

--- kcmkwin/kwinrules/kcmrules.cpp
+++ kcmkwin/kwinrules/kcmrules.cpp
@@ -7,6 +7,8 @@
 
 #include "kcmrules.h"
 
+#include "kdeclarative_version.h"
+
 #include <QDBusConnection>
 #include <QDBusMessage>
 
@@ -45,7 +47,9 @@ KCMKWinRules::KCMKWinRules(QObject *parent, const QVariantList &arguments)
         argList << arg.toString();
     }
     parseArguments(argList);
-
+#if KDECLARATIVE_VERSION >= QT_VERSION_CHECK(5, 80, 0)
+    connect(this, &ConfigModule::argumentsUpdated, this, &KCMKWinRules::parseArguments);
+#endif
     connect(m_rulesModel, &RulesModel::descriptionChanged, this, [this]{
         if (m_editIndex.isValid()) {
             m_ruleBookModel->setDescriptionAt(m_editIndex.row(), m_rulesModel->description());
